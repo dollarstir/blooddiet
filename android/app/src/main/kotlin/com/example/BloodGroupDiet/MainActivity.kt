f07c52e8014fe2bec39d06c608505ed95609fb3c
dollarstir <kpin463@gmail.com>
2022-03-09 09:56:42 +0000

    blood

---
+++ android/app/src/main/kotlin/com/example/BloodGroupDiet/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.BloodGroupDiet
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import './home.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Blood Doctor',
+      debugShowCheckedModeBanner: false,
+      theme: new ThemeData(
+          primaryColor: Color.fromRGBO(58, 66, 86, 1.0), fontFamily: 'Raleway'),
+      home: new HomePage(title: 'Blood Doctor'),
+    );
+  }
+}
